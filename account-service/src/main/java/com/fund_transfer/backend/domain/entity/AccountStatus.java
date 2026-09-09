db2df48641d69aaac04ce35fec93d3d204d47abf
SouvikDev21 <souvik.dey@npstx.com>
2026-09-09 14:54:06 +0530

    Delete account-service/src/main/java/com/fund_transfer/backend/domain directory

--- account-service/src/main/java/com/fund_transfer/backend/domain/entity/AccountStatus.java
+++
@@ -1,5 +0,0 @@
-package com.fund_transfer.backend.domain.entity;
-
-public enum AccountStatus {
-   ACTIVE, FROZEN, DORMANT, CLOSED, INACTIVE, BLOCKED
-}
--- account-service/src/main/java/com/fund_transfer/backend/domain/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fund_transfer.backend.domain.repository;
-
-import com.fund_transfer.backend.domain.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AccountRepository extends JpaRepository<Account, String> {
-   List<Account> findByCifId(String cifId);
-}
